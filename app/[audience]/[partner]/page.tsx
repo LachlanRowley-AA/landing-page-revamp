b7e6f5445d4f25f5f7b65d678ab69abcbb3df930
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-06-16 09:35:16 +1000

    added dynamic routing

--- app/[audience]/[partner]/page.tsx
+++ app/[audience]/[partner]/page.tsx
@@ -1,4 +1,3 @@
-import { UseCases } from '@/components/UseCases/Usecases';
 import { Hero03 } from '@/components/Hero03/index';
 import { Feature02 } from '@/components/feature-02';
 import { Calculator } from '@/components/Calculator/Calculator';
@@ -7,12 +6,17 @@ import { FAQ } from '@/components/FAQ/Faq';
 import { ContactForm } from '@/components/Contact/Contact';
 import { Suspense } from 'react';
 import { UseCases as Benefits} from '@/components/Benefits/Benefits';
+import { audienceContent, AudienceKey } from '@/lib/audienceContent';
 
-export default function HomePage() {
+
+export default function HomePage( {params} : { params: { audience: string}}) {
+  const audience = params.audience as AudienceKey
+
+  const content = audienceContent[audience];
   return (
     <>
       <Suspense><Hero03 /></Suspense>
-      <Feature02 />
+      <Feature02 features={content?.features}/>
       <Calculator />
       {/* <UseCases /> */}
       <Suspense><Benefits /></Suspense>
---
+++ lib/audienceContent.tsx
@@ -0,0 +1,94 @@
+import {
+    IconCurrencyDollar,
+    IconFileOff,
+    IconCalendarTime,
+    IconBuilding,
+    IconClockBolt,
+    IconTrendingUp,
+} from '@tabler/icons-react';
+
+import { ReactNode } from 'react';
+
+export type Feature = {
+    icon: ReactNode,
+    title: string,
+    description: ReactNode
+}
+
+export type AudienceKey = 'fitout' | 'design'
+
+type AudienceContent = {
+    features: Feature[];
+}
+
+export const audienceContent = {
+    design: {
+        features: [
+        {
+            icon: <IconCurrencyDollar size={28} stroke={1.5} />,
+            title: 'Finance up to $300k',
+            description: 'Access unsecured funding of up to $300,000 to fuel your business growth',
+        },
+        {
+            icon: <IconFileOff size={28} stroke={1.5} />,
+            title: 'No Financials Needed',
+            description: 'Get approved without tax returns or financial statements',
+        },
+        {
+            icon: <IconCalendarTime size={28} stroke={1.5} />,
+            title: 'Flexible Terms',
+            description: '5-year loan terms with the option for early payoff',
+        },
+        {
+            icon: <IconBuilding size={28} stroke={1.5} />,
+            title: 'Use for Branding & Design',
+            description: 'Works with all websites — no restrictions',
+        },
+        {
+            icon: <IconClockBolt size={28} stroke={1.5} />,
+            title: 'Fast & Simple',
+            description: 'Fully online application with 2–3 day turnaround',
+        },
+        {
+            icon: <IconTrendingUp size={28} stroke={1.5} />,
+            title: 'Preserve Cashflow',
+            description: 'Build your desired brand without the cashflow constraint',
+        },
+        ],
+    },
+    fitout: {
+        features: [
+            {
+                icon: <IconCurrencyDollar size={28} stroke={1.5} />,
+                title: 'Finance up to $300k',
+                description: 'Access unsecured funding of up to $300,000 to fuel your business growth',
+            },
+            {
+                icon: <IconFileOff size={28} stroke={1.5} />,
+                title: 'No Financials Needed',
+                description: 'Get approved without tax returns or financial statements',
+            },
+            {
+                icon: <IconCalendarTime size={28} stroke={1.5} />,
+                title: 'Flexible Terms',
+                description: '5-year loan terms with the option for early payoff',
+            },
+            {
+                icon: <IconBuilding size={28} stroke={1.5} />,
+                title: 'Use for Fitouts',
+                description: 'Works with all fitouts — no restrictions',
+            },
+            {
+                icon: <IconClockBolt size={28} stroke={1.5} />,
+                title: 'Fast & Simple',
+                description: 'Fully online application with 2–3 day turnaround',
+            },
+            {
+                icon: <IconTrendingUp size={28} stroke={1.5} />,
+                title: 'Preserve Cashflow',
+                description: 'Get your desired fitout without the cashflow constraint',
+            },
+
+        ],
+    },
+} as const satisfies Record<AudienceKey, AudienceContent>;
