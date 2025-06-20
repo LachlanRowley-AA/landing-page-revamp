6765cb35cc1f1a122ccfa8366217a5a046a51f74
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-06-20 15:09:50 +1000

    Added gsv

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -11,7 +11,8 @@ import { Providers } from '../providers';
 export default function AdminLayout({ children }: {children: any}) {
   return (
     <html lang="en" {...mantineHtmlProps}>
-      <body>
+    <meta name="google-site-verification" content="gFnL_l0_VGDHNfIeo7SIS2GvdPEO2YbpG-QGNnmjHBk" />      
+    <body>
         <Providers>
           <MantineProvider theme={theme}>{children}</MantineProvider>          
         </Providers>
