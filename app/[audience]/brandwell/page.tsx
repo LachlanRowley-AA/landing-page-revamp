ee07cc58a8d560d12d4d8bc4b03e11621dab42d0
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-07-03 13:32:05 +1000

    Added desc to brandwell

---
+++ app/[audience]/brandwell/page.tsx
@@ -0,0 +1,24 @@
+import { audienceContent, AudienceKey } from '@/lib/audienceContent';
+import { textKey } from '@/lib/audienceContent';
+import HomePageClient from '@/components/Homepage/HomepageClient';
+
+
+// This is the Server Component
+export default async function HomePage(props: { params: Promise<{ audience: string, partner: string}>}) {
+  const params = await props.params;
+  const audience = params.audience as AudienceKey
+  const partner = 'brandwell';
+  const content = audienceContent[audience];
+  const description = 'monica' as textKey
+  
+  return (
+    <HomePageClient 
+      audience={audience}
+      partner={partner}
+      content={content}
+      calculatorValue={30000}
+      showDescription={true}
+      textKey={description}
+    />
+  );
+}
