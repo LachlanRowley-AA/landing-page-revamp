76c818f34190377a4d3428f63a1554541314b882
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-09-17 11:33:27 +1000

    Added force redirect

--- app/[audience]/[partner]/page.tsx
+++ app/[audience]/[partner]/page.tsx
@@ -1,13 +1,19 @@
 import { audienceContent, AudienceKey } from '@/lib/audienceContent';
 import HomePageClient from '@/components/Homepage/HomepageClient';
-
+import { useRouter } from 'next/navigation';
 // This is the Server Component
 export default async function HomePage(props: { params: Promise<{ audience: string, partner: string}>}) {
   const params = await props.params;
   const audience = params.audience as AudienceKey
   const partner = params.partner
   const content = audienceContent[audience];
+
+  const router = useRouter();
   
+  if(audience === 'calculators'.toLocaleLowerCase()) {
+    router.push(`/calculators/${partner}`)
+  }
+
   return (
     <HomePageClient 
       audience={audience}
